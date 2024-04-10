f10c1a6e3eab92f49790dae5b7f16cfbca3cf04a
Vedant Pandey <vedantpandey434@gmail.com>
2024-04-10 22:44:13 +0530

    Update database connection logic in dbConnect.ts

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -6,6 +6,23 @@ type ConnectionObject = {
 
 const connection: ConnectionObject = {};
 
-async function dbConnect() {
-    
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already connected to DB");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+    console.log(db);
+
+    connection.isConnected = db.connections[0].readyState;
+
+    console.log("DB Connected Successfully");
+  } catch (err) {
+    console.log("Error connecting to DB", err);
+    process.exit(1);
+  }
 }
+
+export default dbConnect;
